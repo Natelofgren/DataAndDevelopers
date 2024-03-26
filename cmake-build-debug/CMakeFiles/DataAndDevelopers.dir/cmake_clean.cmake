32c836dded0ae05b48f92d1d31a3acb144f15cff
isaacstiles4 <isaac.stiles4@gmail.com>
2024-03-26 12:03:19 -0400

    Basic stuff

---
+++ cmake-build-debug/CMakeFiles/DataAndDevelopers.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataAndDevelopers.dir/BaseCode.cpp.obj"
+  "CMakeFiles/DataAndDevelopers.dir/BaseCode.cpp.obj.d"
+  "DataAndDevelopers.exe"
+  "DataAndDevelopers.exe.manifest"
+  "DataAndDevelopers.pdb"
+  "libDataAndDevelopers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataAndDevelopers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DataAndDevelopers.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DataAndDevelopers.
+# This may be replaced when dependencies are built.
