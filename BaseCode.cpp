83e410a35b06c018240c020253c0e974aa730356
Natelofgren <nate@lukesfamily.com>
2024-03-26 11:53:47 -0400

    Made a simple "Hello World!"

--- BaseCode.cpp
+++ BaseCode.cpp
@@ -1,3 +1,11 @@
 //
 // Created by nate on 3/26/2024.
 //
+#include <iostream>
+
+int main() {
+    int x = 1;
+    std::cout << "Hello, World!" << std::endl;
+    printf("Hello \t, %d %d World \n", x,x);
+    return 0;
+}
