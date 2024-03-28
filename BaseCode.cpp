66fa89b94a01cd273d876f8fa2b7b57fee1ea6ad
Natelofgren <nate@lukesfamily.com>
2024-03-28 11:43:08 -0400

    Non-working String code
    work on later

--- BaseCode.cpp
+++ BaseCode.cpp
@@ -3,8 +3,12 @@
 //
 // I'm here, too!!
 #include <iostream>
+#include <string>
 
 int main() {
+    string codeinput = "";
+    std::cout << "Type your adventure (code)"; // Type a line of code and press enter
+    std::cin >> codeinput;  // Get user input from the keyboard
     int x = 1;
     std::cout << "Hello, World!" << std::endl;
     printf("Hello \t, %d %d World \n", x,x);
