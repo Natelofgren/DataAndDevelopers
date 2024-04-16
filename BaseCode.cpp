4781ff553e0e5c5bbfde064d68e64dbe33c4df4e
Natelofgren <nate@lukesfamily.com>
2024-04-16 14:02:58 -0400

    Made it read a print statement and turn it into a token
    reads say, and stores the values

--- BaseCode.cpp
+++ BaseCode.cpp
@@ -6,13 +6,13 @@
 #include <string>
 #include <fstream>
 
-enum token
-{
-    IDENTIFIER = 1,
-    OPERATOR = 2,
-    EOC = 3,
-    EQUALS = 4,
-    NUMBER = 5
+class token {       // The class token
+public:             // Access specifier
+    std::string IDENTIFIER = "IDENTIFIER";
+    std::string OPERATOR = "OPERATOR";
+    std::string EOC = "ENDOFCOMMAND";
+    std::string EQUALS = "EQUALS";
+    std::string VALUE = "VALUE";  // Attribute (string variable)
 };
 
 // tstd::stringhe function below takes a string and sends back the string until the first space
@@ -29,8 +29,18 @@ std::string until_space(std::string const& s)
     }
 }
 
+std::string until_astrix(std::string const& s) {
+    std::string::size_type pos = s.find('*');
+    if (pos != std::string::npos) {
+        return s.substr(0, pos);
+    }
+    else {
+        return s;
+    }
+}
+token tokenizer(std::string line){   //This is where everything is converted to tokens
+    token returntoken;
 
-void tokenizer(std::string line){   //This is where everything is converted to tokens
     if (until_space(line) == "roll"){
     }
     else if (until_space(line) == "nat") {
@@ -40,7 +50,10 @@ void tokenizer(std::string line){   //This is where everything is converted to t
 
     }
     else if (until_space(line) == "say") {
-
+        returntoken.IDENTIFIER = "say";
+        line = line.erase(0, 5);
+        returntoken.VALUE = until_astrix(line);
+        returntoken.EOC = line.back();
     }
     else if (until_space(line) == "quest") {
 
@@ -49,30 +62,37 @@ void tokenizer(std::string line){   //This is where everything is converted to t
 
     }
     else {
-
     }
+    return returntoken;
+}
+
+void parser(token tokenlist[] ) { // This is the final step where we turn the tokens in order
+    std::cout << (tokenlist[0].EOC);
 }
 
 void lexer() { // This is the first step where we get the input from the file
     std::ifstream myfile ("MainDungeon.dandd");
     std::string codeinput = "";
     int countingthelines = 0;
+    token tokenlist[255];
     if ( myfile.is_open() ) { // always check whether the file is open
         while ( myfile ) { // loops until the end of the line
             std::getline(myfile, codeinput); // Grabs a full line of the file
-            countingthelines++;
             if (codeinput == "_^") {
             }
-            else  { // Sends the line to tokenizer to be tokenized!
-                tokenizer(codeinput);
+            else  { // Sends the line to tokenizer to be tokenized and save the result in the list called tokens
+                tokenlist[countingthelines] = (tokenizer(codeinput));
+                countingthelines++;
             }
         }
 
     }
+    parser(tokenlist);
+
+
+
 }
 
-void parser() { // This is the first step where we get the input from the file
-}
 
 int main() {
     lexer();
